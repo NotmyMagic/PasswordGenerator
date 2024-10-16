9fb3fb1a1dba51318bdd46da7d48fc60989249a6
NotmyMagic <56935337+NotmyMagic@users.noreply.github.com>
2024-10-16 12:19:41 -0600

    working password generator for lowercase

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
-const Numbers = "0,1,2,3,4,5,6,7,8,9";
-const Upper = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-const Lower = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const numbers = "0,1,2,3,4,5,6,7,8,9";
 const symbols = "!@#$%^&*()_+{};~?,./";
+const generate = document.getElementById("generate");
+
+generate.addEventListener("click", (e) => {
+  e.preventDefault();
+  generatePass();
+});
 
 function generatePass() {
-  let length = parseInt(document.querySelector("length").value);
+  let length = parseInt(document.querySelector("#length").value);
+  // let upperCheck = document.getElementById("capital:checked");
+  // let numberCheck = document.getElementById("numbers:checked");
+  // let symbolCheck = document.getElementById("symbols:checked");
+  let password = "";
+  let charSet = "";
+  charSet += lower;
+  // if (upperCheck === true) {
+  //   charSet += upper;
+  // }
+  // if (numberCheck === true) {
+  //   charSet += numbers;
+  // }
+  // if (symbolCheck === true) {
+  //   charSet += symbols;
+  // }
+  for (let i = 0; i < length; i++) {
+    let randomIndex = Math.floor(Math.random() * charSet.length);
+    password += charSet[randomIndex];
+  }
+  password = document.getElementById("password").value = password.slice(
+    0,
+    length
+  );
+  console.log(password);
 }
